9d888ed479a6ea0d185b7a56eb5cd216a57db0a2
Luis Enrique De Jesus Vega <ldejesusvega@users.noreply.github.com>
2020-06-27 17:08:39 -0500

    Add HowTo comment description

--- Models/Command.cs
+++ Models/Command.cs
@@ -6,6 +6,9 @@ namespace Commander.Models
         /// Identity Column
         /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// How to define a command
+        /// </summary>
         public string HowTo { get; set; }
         public string Line { get; set; }
         public string Platform { get; set; }
