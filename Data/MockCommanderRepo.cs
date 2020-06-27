7bc7bca9dbe0c4ab39e17b81ecf7eb5f02ff1434
Luis Enrique De Jesus Vega <ldejesusvega@github.com>
2020-06-27 16:46:58 -0500

    fix document format

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -15,9 +15,9 @@ namespace Commander.Data
             var commands = new List<Command>
             {
                 new Command { Id = 0, HowTo = "Boild an egg", Line = "boild water", Platform = "Some text" },
-                new Command   { Id = 1, HowTo = "Huevos a la mexicana", Line = "mezclar huevos con jitomate, cebolla y picante", Platform = "Some text" },
-                new Command{ Id = 2, HowTo = "huevos con jamon", Line = "mezclar huevos con jamon", Platform = "Some text" },
-                new Command{ Id = 3, HowTo = "huevos con salchicha", Line = "mezclar huevos con salchichas", Platform = "Some text" }
+                new Command { Id = 1, HowTo = "Huevos a la mexicana", Line = "mezclar huevos con jitomate, cebolla y picante", Platform = "Some text" },
+                new Command { Id = 2, HowTo = "huevos con jamon", Line = "mezclar huevos con jamon", Platform = "Some text" },
+                new Command { Id = 3, HowTo = "huevos con salchicha", Line = "mezclar huevos con salchichas", Platform = "Some text" }
             };
             return commands;
         }
