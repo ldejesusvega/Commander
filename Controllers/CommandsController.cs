4fab4f94a78aeab15561b550eac016b07bb74a15
Luis Enrique De Jesus Vega <ldejesusvega@users.noreply.github.com>
2020-06-27 17:23:48 -0500

    Define CommandsController como api heredando de Controller Base sinpoder reponder a una vista

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
+    /// api/commands
+    [Route("api/[controller]")]  /// This decoration will build the route based on controller name e.g. api/commands 
+    [ApiController]
     public class CommandsController : ControllerBase
     {
 
