91619b47b1d3dceccf553cb3f2ac2b08f3dd5ee3
Luis Enrique De Jesus Vega <ldejesusvega@users.noreply.github.com>
2020-06-28 01:36:32 -0500

    add comment

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Commander.Controllers
 {
     /// api/commands
-    [Route("api/commands")]  /// This decoration is fixed as api/commands even if the controller name changes 
+    [Route("api/commands")]  /// This decoration is fixed as api/commands even if the controller class name changes 
     [ApiController]
     public class CommandsController : ControllerBase
     {
