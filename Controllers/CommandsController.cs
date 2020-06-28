81af39bede17251199a1079dd5ce97cfbc276958
Luis Enrique De Jesus Vega <ldejesusvega@users.noreply.github.com>
2020-06-28 02:10:58 -0500

    create controller and add movk repo implementation

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -7,6 +10,22 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        /// get api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commands = _repository.GetAllCommands();
+            return Ok(commands);
+        }
+
+        /// get api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var command = _repository.GetCommandById(id);
+            return Ok(command);
+        }
 
     }
 }
