392e1c050d3bbe8627a09bdf302f1c5fc53b0673
Luis Enrique De Jesus Vega <ldejesusvega@users.noreply.github.com>
2020-07-04 02:49:54 -0500

    Agregar por Dependency Injection, SqlCommandRepo y AutoMapper

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,10 @@ namespace Commander
             (Configuration.GetConnectionString("CommanderConnection")));
 
             services.AddControllers();
-            // services.AddScoped<ICommanderRepo, MockCommanderRepo>(); /// Change mockup to SqlCommandRepo
+            // services.AddScoped<ICommanderRepo, MockCommanderRepo>(); /// Change mockup to SqlCommandRepo            
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
